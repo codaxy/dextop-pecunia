62e9d710bb3bc7a232a2e793fe6e1f1d92571492
Michael Hasler <michael.hasler@codaxy.com>
2011-10-05 14:11:24 +0200

    Implementing StockPanel

---
+++ Apps/Pecunia/App/Panels/Stocks/StockPanel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Codaxy.Dextop;
+using Codaxy.Dextop.Remoting;
+using Codaxy.Dextop.Data;
+using System.Xml.Linq;
+using System.Globalization;
+using System.Xml;
+using Codaxy.Dextop.Forms;
+using Pecunia.Services;
+using Codaxy.Common;
+
+namespace Pecunia.App
+{
+    public class StockPanel : DextopWindow
+    {
+
+        [DextopRemotableConstructor(alias = "courses")]
+        public StockPanel()
+        {
+            
+        }
+
+        public override void InitRemotable(DextopRemote remote, DextopConfig config)
+        {
+            base.InitRemotable(remote, config);			
+            Remote.AddStore("model", Load);
+		
+        }
+
+		Stock[] Load(DextopReadFilter filter)
+		{			
+			/*
+			var data = CurrencyService.GetCurrencyList(currency);
+			return data.Rates.Select(a => new RateModel
+			{
+				Currency = a.Currency,
+				ISOCode = a.ISOCode,
+				Rate = a.Rate,
+				Amount = amount * a.Rate
+			}).ToArray();
+            */ 
+		}
+
+        CurrencyHistoryRate[] LoadHistory(DextopReadFilter filter) 
+        {
+            String iso;
+            var currency = filter.Params.TryGet<String>("ISOCode", out iso);
+
+            return CurrencyHistoryService.getHistoryRateOfCurrency(iso).ToArray();
+        }
+
+		[DextopModel]
+		[DextopGrid]
+		class Stock
+		{
+			[DextopGridColumn(width = 200, text = "Name")]
+			public String Name { get; set; }
+
+			[DextopGridColumn(width = 100, text = "Value")]
+			public decimal? Value { get; set; }
+		}     
+      
+    }
+        
+}
---
+++ Apps/Pecunia/App/Panels/Stocks/StockPanel.js
@@ -0,0 +1,138 @@
+﻿Ext.define('Pecunia.StockPanel', {
+    extend: 'Dextop.Panel',
+
+    title: 'Courses',
+    border: true,
+    closable: true,
+
+    initComponent: function () {
+
+        var historyStore = this.remote.createStore("history");
+
+        var grid = Ext.create('Dextop.ux.SwissArmyGrid', {
+            region: 'center',
+            remote: this.remote,
+            border: true,
+            margins: '0 0 -1 -1',
+            margins: '5',
+            paging: false,
+            storeOptions: {
+                autoLoad: true
+            },
+            listeners: {
+                itemclick: function (view, record) {
+                    historyStore.load({
+                        params: { ISOCode : record.get("ISOCode") }
+                    });
+                }
+            }
+
+        });
+
+        // ---
+
+        var formFields = Ext.create(this.getNestedTypeName('.form.ConvertForm')).getItems({
+            remote: this.remote,
+            data: this.convertData,
+            apply: {
+                Amount: {
+                    listeners: {
+                        scope: this,
+                        'specialkey': function (field, e) {
+                            if (e.getKey() == e.ENTER)
+                                this.recalculate();
+                        }
+                    }
+                }
+            }
+        });
+
+        formFields.push({
+            text: 'Recalculate',
+            xtype: 'button',
+            margin: '0 0 0 5',
+            height: 35,
+            scope: this,
+            handler: this.recalculate = function () {
+                var form = this.down('form');
+                if (!form.getForm().isValid())
+                    return;
+                grid.store.load({
+                    params: form.getForm().getFieldValues()
+                });
+            }
+        });
+
+        var calcForm = Ext.create('Ext.form.Panel', {
+            height: 60,
+            region: 'north',
+            border: false,
+            layout: {
+                type: 'hbox',
+                align: 'middle'
+            },
+            fieldDefaults: {
+                margin: '0 0 0 5'
+            },
+            bodyStyle: 'padding: 5px',
+            items: formFields
+        });
+
+
+        var chart = Ext.create('Ext.chart.Chart', {
+            region: 'center',
+            width: 400,
+            height: 300,
+            store: historyStore,
+            axes: [
+            {
+                title: 'Value',
+                type: 'Numeric',
+                position: 'left',
+                fields: ['Rate']
+            },
+            {
+                title: 'Date',
+                type: 'Time',
+                dateFormat: 'd M Y',
+                position: 'bottom',
+                fields: ['Date']
+            }
+            ],
+            series: [
+            {
+                type: 'line',
+                xField: 'Date',
+                yField: 'Rate'
+            }
+            ]
+        });
+
+
+        var _self = this;
+        Ext.apply(this, {
+            layout: 'border',
+            items: [{
+                border: false,
+                region: 'west',
+                width: 600,
+                layout: 'border',
+                items: [grid, calcForm]
+            }, chart],
+            buttons: [{
+                text: 'Callback Function',
+                handler: function () {
+                    _self.remote.getCurrencyHistoryRate("AUD", function (r) {
+                        if (r && r.success)
+                            store.loadData(r.result);
+                        else
+                            alert('failed');
+                    }, _self);
+                }
+            }]
+        });
+
+        this.callParent(arguments);
+
+    }
+});
