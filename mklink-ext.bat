b2645cfcbfa941ae5322cca644ff62e67c501d8e
Marko Stijak <mstijak@gmail.com>
2012-03-15 10:46:38 +0100

    extjs 4.1.0 rc1

--- mklink-ext.bat
+++ mklink-ext.bat
@@ -6,7 +6,7 @@
 REM Replace with valid path to Ext
 
 rd Apps\Pecunia\client\lib\ext
-mklink /D Apps\Pecunia\client\lib\ext C:\Code\Lib\Ext\ext-4.1.0-beta-3
+mklink /D Apps\Pecunia\client\lib\ext C:\Code\Lib\Ext\extjs-4.1.0-rc1
 
 @pause
 @popd
